cde7ae2ca3b7d3a68decadf6195bf98a792c2a29
kevin hu <hu.kevin.w@gmail.com>
2018-01-18 16:23:30 -0500

    Import Login component to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import Login from './components/users/login';
+
 // import registerServiceWorker from './registerServiceWorker';
 const Main = () => {
 	return (
 		<BrowserRouter>
 			<div className="app">
 				<Route exact path="/" component={App} />
+				<Route exact path="/login" component={Login} />
 			</div>
 		</BrowserRouter>
 	)
